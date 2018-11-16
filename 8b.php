fbee0f2f5ccbd0a15c6403e17e7dc0799f2acd23
Shivaprasad K <shiva15997@gmail.com>
2018-11-16 12:04:16 +0530

    Update 8b.php

--- 8b.php
+++ 8b.php
@@ -11,14 +11,14 @@ for($row=0;$row<3;$row++){
 	echo "<center><p>".$trans[$row][$col]."</p></center>";
 }
 }
-array_unshift ($trans,null);
-$trans=call_user_func_array('array_map',$trans);
+
+
 echo "<h2> transpose of matrix </h2>";
 for($row=0;$row<3;$row++){
 	echo "<center><p><b>row number $row</b></p></center>";
 	for($col=0;$col<3;$col++)
 	{
-	echo "<center><p>".$trans[$row][$col]."</p></center>";
+	echo "<center><p>".$trans[$col][$row]."</p></center>";
 }
 }
 ?>
